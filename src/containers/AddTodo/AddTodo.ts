3e21e5b18fb28483de0eb0222467f15625b442cf
Yuta Takahashi <b1015143@fun.ac.jp>
2018-12-01 20:15:21 +0900

    fix: dispatch type

--- src/containers/AddTodo/AddTodo.ts
+++ src/containers/AddTodo/AddTodo.ts
@@ -3,8 +3,9 @@ import { addTodo } from 'src/actions/Todo/TodoActionCreator';
 import AddTodo from 'src/components/AddTodo/AddTodo';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
+import TodoAction from 'src/actions/Todo/TodoAction';
 
-const mapDispatchToProps = (dispatch: Dispatch): IAddTodoDispatchProps => {
+const mapDispatchToProps = (dispatch: Dispatch<TodoAction>): IAddTodoDispatchProps => {
     return {
         addTodo: (todoText: string) => {
             dispatch(addTodo(todoText));
