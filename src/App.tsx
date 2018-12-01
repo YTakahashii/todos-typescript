22a1d8d8d3e88831c1f8cfe3a0083ef80babca30
Yuta Takahashi <b1015143@fun.ac.jp>
2018-12-01 23:32:15 +0900

    feature: render TodoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import './App.css';
 import AddTodo from './containers/AddTodo/AddTodo';
+import VisibleTodoList from './containers/(Visible)TodoList/TodoList';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
         <AddTodo />
+        <VisibleTodoList />
       </div>
     );
   }
